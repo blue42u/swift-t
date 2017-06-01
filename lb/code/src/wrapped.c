f9c352549b5bb2ea6d2a9465e3d6ab9ed8adfdcf
blue42u <blue42u@users.noreply.github.com>
2017-06-01 16:05:30 -0500

    Added compile-time DEBUG option, for the wrapper

--- lb/code/src/wrapped.c
+++ lb/code/src/wrapped.c
@@ -1,6 +1,8 @@
 #include "adlb-x.h"
 #include <stdio.h>
 
+#define DEBUG 0
+
 /*
    These are the functions available to ADLB application code
  */
@@ -8,41 +10,55 @@ adlb_code ADLB_Init(int nservers, int ntypes, int type_vect[],
                     int *am_server, MPI_Comm adlb_comm,
                     MPI_Comm *worker_comm) {
 	adlb_code r = ADLBX_Init(nservers, ntypes, type_vect, am_server, adlb_comm, worker_comm);
+#if DEBUG
 	printf("Init %x %x\n", adlb_comm, *worker_comm);
+#endif
 	return r;
 }
 
 adlb_code ADLB_Server(long max_memory) {
+#if DEBUG
 	printf("Server\n");
+#endif
 	return ADLBX_Server(max_memory);
 }
 
 adlb_code ADLB_Version(version* output) {
+#if DEBUG
 	printf("Version\n");
+#endif
 	return ADLBX_Version(output);
 }
 
 MPI_Comm ADLB_GetComm_workers(void) {
 	MPI_Comm c = ADLBX_GetComm_workers();
+#if DEBUG
 	printf("GetComm_workers %x\n", c);
+#endif
 	return c;
 }
 
 MPI_Comm ADLB_GetComm_leaders(void) {
 	MPI_Comm c = ADLBX_GetComm_leaders();
+#if DEBUG
 	printf("GetComm_leaders %x\n", c);
+#endif
 	return c;
 }
 
 adlb_code ADLB_Hostmap_stats(unsigned int* count,
                              unsigned int* name_max) {
+#if DEBUG
 	printf("Hostmap_stats\n");
+#endif
 	return ADLBX_Hostmap_stats(count, name_max);
 }
 
 adlb_code ADLB_Hostmap_lookup(const char* name, int count,
                               int* output, int* actual) {
+#if DEBUG
 	printf("Hostmap_lookup\n");
+#endif
 	return ADLBX_Hostmap_lookup(name, count, output, actual);
 }
 
@@ -55,7 +71,9 @@ adlb_code ADLB_Hostmap_lookup(const char* name, int count,
  */
 adlb_code ADLB_Hostmap_list(char* output, unsigned int max,
                             unsigned int offset, int* actual) {
+#if DEBUG
 	printf("Hostmap_list\n");
+#endif
 	return ADLBX_Hostmap_list(output, max, offset, actual);
 }
 
@@ -74,7 +92,9 @@ adlb_code ADLB_Hostmap_list(char* output, unsigned int max,
  */
 adlb_code ADLB_Put(const void* payload, int length, int target, int answer,
                    int type, adlb_put_opts opts) {
+#if DEBUG
 	printf("Put\n");
+#endif
 	return ADLBX_Put(payload, length, target, answer, type, opts);
 }
 
@@ -92,7 +112,9 @@ adlb_code ADLB_Dput(const void* payload, int length, int target,
         int answer, int type, adlb_put_opts opts, const char *name,
         const adlb_datum_id *wait_ids, int wait_id_count, 
         const adlb_datum_id_sub *wait_id_subs, int wait_id_sub_count) {
+#if DEBUG
 	printf("Dput\n");
+#endif
 	return ADLBX_Dput(payload, length, target, answer, type, opts, name, wait_ids, wait_id_count, wait_id_subs, wait_id_sub_count);
 }
 
@@ -117,7 +139,9 @@ adlb_code ADLB_Get(int type_requested, void** payload,
 	MPI_Comm tmp;
 	if(comm == NULL) comm = &tmp;
 	adlb_code r = ADLBX_Get(type_requested, payload, length, max_length, answer, type_recvd, comm);
+#if DEBUG
 	printf("Get %x\n", *comm);
+#endif
 	return r;
 }
 
@@ -131,7 +155,9 @@ adlb_code ADLB_Get(int type_requested, void** payload,
 adlb_code ADLB_Iget(int type_requested, void* payload, int* length,
                     int* answer, int* type_recvd, MPI_Comm* comm) {
 	adlb_code r = ADLBX_Iget(type_requested, payload, length, answer, type_recvd, comm);
+#if DEBUG
 	printf("Iget %x\n", *comm);
+#endif
 	return r;
 }
 /*
@@ -150,7 +176,9 @@ adlb_code ADLB_Iget(int type_requested, void* payload, int* length,
  */
 adlb_code ADLB_Aget(int type_requested, adlb_payload_buf payload,
                      adlb_get_req *req) {
+#if DEBUG
 	printf("Aget\n");
+#endif
 	return ADLBX_Aget(type_requested, payload, req);
 }
 
@@ -168,7 +196,9 @@ adlb_code ADLB_Aget(int type_requested, adlb_payload_buf payload,
 adlb_code ADLB_Amget(int type_requested, int nreqs, bool wait,
                      const adlb_payload_buf* payloads,
                      adlb_get_req *reqs) {
+#if DEBUG
 	printf("Amget\n");
+#endif
 	return ADLBX_Amget(type_requested, nreqs, wait, payloads, reqs);
 }
 
@@ -179,7 +209,9 @@ adlb_code ADLB_Amget(int type_requested, int nreqs, bool wait,
  */
 adlb_code ADLB_Aget_test(adlb_get_req *req, int* length,
                     int* answer, int* type_recvd, MPI_Comm* comm) {
+#if DEBUG
 	printf("Aget_test\n");
+#endif
 	return ADLBX_Aget_test(req, length, answer, type_recvd, comm);
 }
 
@@ -190,7 +222,9 @@ adlb_code ADLB_Aget_test(adlb_get_req *req, int* length,
 adlb_code ADLB_Aget_wait(adlb_get_req *req, int* length,
                     int* answer, int* type_recvd, MPI_Comm* comm) {
 	adlb_code r = ADLBX_Aget_wait(req, length, answer, type_recvd, comm);
+#if DEBUG
 	printf("Aget_wait %x\n", *comm);
+#endif
 	return r;
 }
 
@@ -198,7 +232,9 @@ adlb_code ADLB_Aget_wait(adlb_get_req *req, int* length,
    Obtain server rank responsible for data id
  */
 int ADLB_Locate(adlb_datum_id id) {
+#if DEBUG
 	printf("Locate\n");
+#endif
 	return ADLBX_Locate(id);
 }
 
@@ -208,7 +244,9 @@ int ADLB_Locate(adlb_datum_id id) {
 adlb_code ADLB_Create(adlb_datum_id id, adlb_data_type type,
                       adlb_type_extra type_extra,
                       adlb_create_props props, adlb_datum_id *new_id) {
+#if DEBUG
 	printf("Create\n");
+#endif
 	return ADLBX_Create(id, type, type_extra, props, new_id);
 }
 
@@ -217,38 +255,50 @@ adlb_code ADLB_Create(adlb_datum_id id, adlb_data_type type,
 // will be filled in with a new id
 // Can be called locally on server where data resides
 adlb_code ADLB_Multicreate(ADLB_create_spec *specs, int count) {
+#if DEBUG
 	printf("Multicreate\n");
+#endif
 	return ADLBX_Multicreate(specs, count);
 }
 
 
 adlb_code ADLB_Create_integer(adlb_datum_id id, adlb_create_props props,
                               adlb_datum_id *new_id) {
+#if DEBUG
 	printf("Create_integer\n");
+#endif
 	return ADLBX_Create_integer(id, props, new_id);
 }
 
 adlb_code ADLB_Create_float(adlb_datum_id id, adlb_create_props props,
                               adlb_datum_id *new_id) {
+#if DEBUG
 	printf("Create_float\n");
+#endif
 	return ADLBX_Create_float(id, props, new_id);
 }
 
 adlb_code ADLB_Create_string(adlb_datum_id id, adlb_create_props props,
                               adlb_datum_id *new_id) {
+#if DEBUG
 	printf("Create_string\n");
+#endif
 	return ADLBX_Create_string(id, props, new_id);
 }
 
 adlb_code ADLB_Create_blob(adlb_datum_id id, adlb_create_props props,
                               adlb_datum_id *new_id) {
+#if DEBUG
 	printf("Create_blob\n");
+#endif
 	return ADLBX_Create_blob(id, props, new_id);
 }
 
 adlb_code ADLB_Create_ref(adlb_datum_id id, adlb_create_props props,
                               adlb_datum_id *new_id) {
+#if DEBUG
 	printf("Create_ref\n");
+#endif
 	return ADLBX_Create_ref(id, props, new_id);
 }
 /**
@@ -257,7 +307,9 @@ adlb_code ADLB_Create_ref(adlb_datum_id id, adlb_create_props props,
  */
 adlb_code ADLB_Create_struct(adlb_datum_id id, adlb_create_props props,
                              adlb_struct_type struct_type, adlb_datum_id *new_id) {
+#if DEBUG
 	printf("Create_struct\n");
+#endif
 	return ADLBX_Create_struct(id, props, struct_type, new_id);
 }
 
@@ -266,7 +318,9 @@ adlb_code ADLB_Create_container(adlb_datum_id id,
                                 adlb_data_type val_type, 
                                 adlb_create_props props,
                                 adlb_datum_id *new_id) {
+#if DEBUG
 	printf("Create_container\n");
+#endif
 	return ADLBX_Create_container(id, key_type, val_type, props, new_id);
 }
 
@@ -274,7 +328,9 @@ adlb_code ADLB_Create_multiset(adlb_datum_id id,
                                 adlb_data_type val_type, 
                                 adlb_create_props props,
                                 adlb_datum_id *new_id) {
+#if DEBUG
 	printf("Create_multiset\n");
+#endif
 	return ADLBX_Create_multiset(id, val_type, props, new_id);
 }
 /*
@@ -285,7 +341,9 @@ adlb_code ADLB_Create_multiset(adlb_datum_id id,
   data: associated null-terminated data string, will be copied.
  */
 adlb_code ADLB_Add_dsym(adlb_dsym symbol, adlb_dsym_data data) {
+#if DEBUG
 	printf("Add_dsym\n");
+#endif
 	return ADLBX_Add_dsym(symbol, data);
 }
 
@@ -296,13 +354,17 @@ adlb_code ADLB_Add_dsym(adlb_dsym symbol, adlb_dsym_data data) {
   return: entry previous added for symbol, or NULL values if not present
  */
 adlb_dsym_data ADLB_Dsym(adlb_dsym symbol) {
+#if DEBUG
 	printf("Dsym\n");
+#endif
 	return ADLBX_Dsym(symbol);
 }
 
 adlb_code ADLB_Exists(adlb_datum_id id, adlb_subscript subscript, bool* result,
                        adlb_refc decr) {
+#if DEBUG
 	printf("Exists\n");
+#endif
 	return ADLBX_Exists(id, subscript, result, decr);
 }
 
@@ -317,7 +379,9 @@ adlb_code ADLB_Exists(adlb_datum_id id, adlb_subscript subscript, bool* result,
  */
 adlb_code ADLB_Refcount_get(adlb_datum_id id, adlb_refc *result,
                               adlb_refc decr) {
+#if DEBUG
 	printf("Refcount_get\n");
+#endif
 	return ADLBX_Refcount_get(id, result, decr);
 }
 
@@ -335,7 +399,9 @@ adlb_code ADLB_Refcount_get(adlb_datum_id id, adlb_refc *result,
 adlb_code ADLB_Store(adlb_datum_id id, adlb_subscript subscript,
           adlb_data_type type, const void *data, size_t length,
           adlb_refc refcount_decr, adlb_refc store_refcounts) {
+#if DEBUG
 	printf("Store\n");
+#endif
 	return ADLBX_Store(id, subscript, type, data, length, refcount_decr, store_refcounts);
 }
 
@@ -353,7 +419,9 @@ adlb_code ADLB_Store(adlb_datum_id id, adlb_subscript subscript,
 adlb_code ADLB_Retrieve(adlb_datum_id id, adlb_subscript subscript,
       adlb_retrieve_refc refcounts, adlb_data_type* type,
       void* data, size_t* length) {
+#if DEBUG
 	printf("Retrieve\n");
+#endif
 	return ADLBX_Retrieve(id, subscript, refcounts, type, data, length);
 }
 
@@ -374,18 +442,24 @@ adlb_code ADLB_Enumerate(adlb_datum_id container_id,
                    bool include_keys, bool include_vals,
                    void** data, size_t* length, int* records,
                    adlb_type_extra *kv_type) {
+#if DEBUG
 	printf("Enumerate\n");
+#endif
 	return ADLBX_Enumerate(container_id, count, offset, decr, include_keys, include_vals, data, length, records, kv_type);
 }
 
 // Switch on read refcounting and memory management, which is off by default
 adlb_code ADLB_Read_refcount_enable(void) {
+#if DEBUG
 	printf("Read_refcount_enable\n");
+#endif
 	return ADLBX_Read_refcount_enable();
 }
 
 adlb_code ADLB_Refcount_incr(adlb_datum_id id, adlb_refc change) {
+#if DEBUG
 	printf("Refcount_incr\n");
+#endif
 	return ADLBX_Refcount_incr(id, change);
 }
 
@@ -406,7 +480,9 @@ adlb_code ADLB_Insert_atomic(adlb_datum_id id, adlb_subscript subscript,
                         adlb_retrieve_refc refcounts,
                         bool *result, bool *value_present,
                         void *data, size_t *length, adlb_data_type *type) {
+#if DEBUG
 	printf("Insert_atomic\n");
+#endif
 	return ADLBX_Insert_atomic(id, subscript, refcounts, result, value_present, data, length, type);
 }
 
@@ -416,7 +492,9 @@ adlb_code ADLB_Insert_atomic(adlb_datum_id id, adlb_subscript subscript,
  */
 adlb_code ADLB_Subscribe(adlb_datum_id id, adlb_subscript subscript,
                           int work_type, int* subscribed) {
+#if DEBUG
 	printf("Subscribe\n");
+#endif
 	return ADLBX_Subscribe(id, subscript, work_type, subscribed);
 }
 
@@ -424,7 +502,9 @@ adlb_code ADLB_Container_reference(adlb_datum_id id, adlb_subscript subscript,
                 adlb_datum_id ref_id, adlb_subscript ref_subscript,
                 adlb_data_type ref_type, adlb_refc transfer_refs,
                 int ref_write_decr) {
+#if DEBUG
 	printf("Container_reference\n");
+#endif
 	return ADLBX_Container_reference(id, subscript, ref_id, ref_subscript, ref_type, transfer_refs, ref_write_decr);
 }
 
@@ -432,7 +512,9 @@ adlb_code ADLB_Container_reference(adlb_datum_id id, adlb_subscript subscript,
  * Allocate a unique data ID
  */
 adlb_code ADLB_Unique(adlb_datum_id *result) {
+#if DEBUG
 	printf("Unique\n");
+#endif
 	return ADLBX_Unique(result);
 }
 
@@ -453,34 +535,46 @@ adlb_code ADLB_Unique(adlb_datum_id *result) {
  * with any IDs already created, this will return an error.
  */
 adlb_code ADLB_Alloc_global(int count, adlb_datum_id *start) {
+#if DEBUG
 	printf("Alloc_global\n");
+#endif
 	return ADLBX_Alloc_global(count, start);
 }
 
 adlb_code ADLB_Typeof(adlb_datum_id id, adlb_data_type* type) {
+#if DEBUG
 	printf("Typeof\n");
+#endif
 	return ADLBX_Typeof(id, type);
 }
 
 adlb_code ADLB_Container_typeof(adlb_datum_id id, adlb_data_type* key_type,
                                  adlb_data_type* val_type) {
+#if DEBUG
 	printf("Container_typeof\n");
+#endif
 	return ADLBX_Container_typeof(id, key_type, val_type);
 }
 
 adlb_code ADLB_Container_size(adlb_datum_id container_id, int* size,
                               adlb_refc decr) {
+#if DEBUG
 	printf("Container_size\n");
+#endif
 	return ADLBX_Container_size(container_id, size, decr);
 }
 
 adlb_code ADLB_Lock(adlb_datum_id id, bool* result) {
+#if DEBUG
 	printf("Lock\n");
+#endif
 	return ADLBX_Lock(id, result);
 }
 
 adlb_code ADLB_Unlock(adlb_datum_id id) {
+#if DEBUG
 	printf("Unlock\n");
+#endif
 	return ADLBX_Unlock(id);
 }
 
@@ -490,12 +584,16 @@ adlb_code ADLB_Unlock(adlb_datum_id id) {
  */
 adlb_code ADLB_Data_string_totype(const char* type_string,
               adlb_data_type* type, adlb_type_extra *extra) {
+#if DEBUG
 	printf("Data_string_totype\n");
+#endif
 	return ADLBX_Data_string_totype(type_string, type, extra);
 }
 
 const char *ADLB_Data_type_tostring(adlb_data_type type) {
+#if DEBUG
 	printf("Data_type_tostring\n");
+#endif
 	return ADLBX_Data_type_tostring(type);
 }
 
@@ -505,18 +603,24 @@ const char *ADLB_Data_type_tostring(adlb_data_type type) {
  */
 adlb_code ADLB_string_to_placement(const char *string,
                            adlb_placement *placement) {
+#if DEBUG
 	printf("string_to_placement\n");
+#endif
 	return ADLBX_string_to_placement(string, placement);
 }
 
 adlb_code ADLB_Server_idle(int rank, int64_t check_attempt, bool* result,
                  int *request_counts, int *untargeted_work_counts) {
+#if DEBUG
 	printf("Server_idle\n");
+#endif
 	return ADLBX_Server_idle(rank, check_attempt, result, request_counts, untargeted_work_counts);
 }
 
 adlb_code ADLB_Finalize(void) {
+#if DEBUG
 	printf("Finalize\n");
+#endif
 	return ADLBX_Finalize();
 }
 
@@ -524,11 +628,15 @@ adlb_code ADLB_Finalize(void) {
    Tell server to fail.
  */
 adlb_code ADLB_Fail(int code) {
+#if DEBUG
 	printf("Fail\n");
+#endif
 	return ADLBX_Fail(code);
 }
 
 void ADLB_Abort(int code) {
+#if DEBUG
 	printf("Abort\n");
+#endif
 	ADLBX_Abort(code);
 }
